30b108fcf27f52fc68f2ee2c19f361a0f7396353
owtu16 <hombrepajaro@mail.fresnostate.edu>
2021-10-13 19:53:08 -0700

    Add files via upload

---
+++ 100days/day2/ex1.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+line = str(two_digit_number)
+
+result = int(line[0]) + int(line[1])
+
+print(line[0] + " + " + line[1] + " = " + str(result))
+print(result)
+
+
