210712688901986b2812930348b16988bd49b4c6
owtu16 <hombrepajaro@mail.fresnostate.edu>
2021-10-13 20:27:35 -0700

    Add files via upload

---
+++ 100days/day2/v21.py
@@ -0,0 +1,30 @@
+
+
+print(round(8/3))
+
+print(round(8/3, 2))
+
+print(type(8 // 3))
+
+print(4/2)
+
+result = 4/2
+
+result /= 2
+
+print(result)
+
+
+score = 0 
+height = 1.8
+isWinning = True
+# User Scores a point
+
+score += 1
+print(score)
+
+print("Your score is " + str(score))
+
+#f-string
+
+print(f"Your score is {score}, your height is {height} and you are winning is {isWinning}")
