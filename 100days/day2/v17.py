c957089b232bec738c979d43e58f86702a7c84c0
owtu16 <hombrepajaro@mail.fresnostate.edu>
2021-10-13 19:36:33 -0700

    Add files via upload

---
+++ 100days/day2/v17.py
@@ -0,0 +1,24 @@
+#Data Types
+
+# String
+
+print("Hello"[0])
+
+print("Hello"[4])
+
+for i in "Hello":
+  print(i)
+
+# Integer
+print(123+456)
+
+
+# Float
+
+3.141592
+
+# Boolean 
+
+True 
+False
+
