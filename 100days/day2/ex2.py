64d42a788efadeb5d88ae8333877131f516a2f76
owtu16 <hombrepajaro@mail.fresnostate.edu>
2021-10-13 20:18:19 -0700

    Add files via upload

---
+++ 100days/day2/ex2.py
@@ -0,0 +1,25 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+height = float(height)
+weight = float(weight)
+
+bmi = (weight/(height ** 2))
+
+print(str(bmi))
+
+
+
+# Angela's code below
+
+bmi_a = int(weight) / float(height) ** 2
+bmi_as_int = int(bmi) 
+print(bmi_as_int)
+
+
+
+
